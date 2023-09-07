3006bf0c983ea053d4fbdba112765e8270176f9b
Ukeme Edet <ukemeedet2207@gmail.com>
2023-09-07 22:39:52 +0100

    Done all :)

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<unistd.h>
+
+/**
+ * main - The entry point for the program
+ * Return: 0 if the program is successful, else 1
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * main - Program Entry
+ * Return: 0 if the program is succesful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
