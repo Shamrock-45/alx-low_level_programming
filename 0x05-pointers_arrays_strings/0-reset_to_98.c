82d76d61a4c6017dbfc2e3e9e1b614161545c3c9
Ukeme Edet <92282594+Ukeme-Edet@users.noreply.github.com>
2023-09-18 20:57:16 +0100

    HOW'S YOUR PC?

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Takes a pointer and updates th3 value it points to to 98
+ * @n: The pointer to the int whose value is to be reset to 98
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
